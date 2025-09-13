776cd9fd80611d92fd295ed0ff6b6797644ddee8
Ernesto Espinoza Menocal <emenocal07@gmail.com>
2025-09-13 13:09:33 +0200

    Add testing modules and config

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+import App from '../App'
+
+describe('App', () => {
+  test('Text renders correctly on App', () => {
+    const { getByText } = render(<App />)
+
+    getByText('Countries Explorer App!')
+  })
+})
