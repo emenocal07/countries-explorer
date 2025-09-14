0a3c0d04886b4578a66f97f0b7129a449eb9977d
Ernesto Espinoza Menocal <emenocal07@gmail.com>
2025-09-14 22:15:26 +0200

    Add components

---
+++ src/components/StyledButton/StyledButton.styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  wrapper: {
+    position: 'relative',
+    alignItems: 'center',
+  },
+  measureText: {
+    position: 'absolute',
+    opacity: 0,
+    left: 0,
+    top: 0,
+    fontSize: 16,
+    paddingHorizontal: 16,
+  },
+})
