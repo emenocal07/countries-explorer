e7aad76c216ba931f9eaeec2ae4ecda24105472d
Ernesto Espinoza Menocal <emenocal07@gmail.com>
2025-09-14 23:08:46 +0200

    Add Home screen

--- src/screens/HomeScreen/HomeScreen.styles.ts
+++ src/screens/HomeScreen/HomeScreen.styles.ts
@@ -5,10 +5,31 @@ export const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: '5%',
+    width: '100%',
   },
   title: {
-    fontSize: 24,
-    fontWeight: 'bold',
     marginBottom: 16,
+    width: '100%',
+    textAlign: 'center',
+  },
+  languageSelector: {
+    position: 'absolute',
+    top: 16,
+    right: 16,
+    zIndex: 10,
+  },
+  descriptionWrapper: {
+    width: '100%',
+    marginBottom: 16,
+  },
+  descriptionText: {
+    textAlign: 'center',
+    width: '100%',
+  },
+  buttonWrapper: {
+    marginTop: 32,
+    width: '100%',
+    alignItems: 'center',
   },
 })
