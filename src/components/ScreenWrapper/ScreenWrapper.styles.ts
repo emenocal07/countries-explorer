b3d304ce993f74255961b6dc37e8b22ddb41d753
Ernesto Espinoza Menocal <emenocal07@gmail.com>
2025-09-16 01:25:14 +0200

    Add screen wrapper component

---
+++ src/components/ScreenWrapper/ScreenWrapper.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 32,
+    paddingBottom: 12,
+    backgroundColor: '#fff',
+  },
+})
