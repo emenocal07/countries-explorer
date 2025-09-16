19fea72f964c5e68da64578db7a2d920234914d7
Ernesto Espinoza Menocal <emenocal07@gmail.com>
2025-09-16 20:11:52 +0200

    Fix tests

--- src/components/LanguageSelector/__tests__/LanguageSelector.test.tsx
+++ src/components/LanguageSelector/__tests__/LanguageSelector.test.tsx
@@ -1,5 +1,6 @@
-import { render } from '@testing-library/react-native'
+import { fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
+import { PaperProvider } from 'react-native-paper'
 import LanguageSelector from '../LanguageSelector'
 
 jest.mock('react-i18next', () => ({
@@ -11,7 +12,12 @@ jest.mock('react-i18next', () => ({
 
 describe('LanguageSelector', () => {
   it('renders language options', () => {
-    const { getAllByText } = render(<LanguageSelector />)
+    const { getByRole, getAllByText } = render(
+      <PaperProvider>
+        <LanguageSelector />
+      </PaperProvider>
+    )
+    fireEvent.press(getByRole('button'))
     expect(getAllByText(/English|Español/)).toBeTruthy()
   })
 })
--- src/components/ScreenWrapper/__tests__/ScreenWrapper.test.tsx
+++ src/components/ScreenWrapper/__tests__/ScreenWrapper.test.tsx
@@ -1,14 +1,31 @@
 import { render } from '@testing-library/react-native'
 import React from 'react'
 import { Text } from 'react-native'
+import { PaperProvider } from 'react-native-paper'
 import { ScreenWrapper } from '../ScreenWrapper'
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({
+    i18n: { language: 'en-US', changeLanguage: jest.fn() },
+    t: (key: string) => key,
+  }),
+}))
+
+jest.mock('react-native-paper/src/components/MaterialCommunityIcon', () => {
+  return {
+    __esModule: true,
+    default: (props: any) => null,
+  }
+})
 
 describe('ScreenWrapper', () => {
   it('renders children correctly', () => {
     const { getByText } = render(
-      <ScreenWrapper>
-        <Text>Child content</Text>
-      </ScreenWrapper>
+      <PaperProvider>
+        <ScreenWrapper>
+          <Text>Child content</Text>
+        </ScreenWrapper>
+      </PaperProvider>
     )
     expect(getByText('Child content')).toBeTruthy()
   })
