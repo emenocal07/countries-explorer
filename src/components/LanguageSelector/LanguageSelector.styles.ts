04fd9133f04a67071889151d52a0ad2789ff2da3
Ernesto Espinoza Menocal <emenocal07@gmail.com>
2025-09-16 19:49:44 +0200

    Refactor language selector component

--- src/components/LanguageSelector/LanguageSelector.styles.ts
+++ src/components/LanguageSelector/LanguageSelector.styles.ts
@@ -1,15 +1,38 @@
 import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
-  container: {
-    padding: 0,
+  fab: {
+    position: 'absolute',
+    top: 24,
+    right: 16,
+    backgroundColor: '#6B4FAA',
+    elevation: 4,
+    zIndex: 100,
   },
-  title: {
-    marginBottom: 8,
+  modal: {
+    backgroundColor: '#fff',
+    margin: 32,
+    borderRadius: 20,
+    padding: 24,
+    alignItems: 'center',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.15,
+    shadowRadius: 6,
+    elevation: 8,
+  },
+  container: {
+    width: '100%',
+    alignItems: 'center',
   },
   segmented: {
-    marginTop: 8,
-    minWidth: 190,
-    alignSelf: 'center',
+    borderRadius: 20,
+    paddingHorizontal: 8,
+    marginTop: 4,
+    width: '100%',
+  },
+  closeFab: {
+    marginTop: 16,
+    backgroundColor: '#6B4FAA',
+    elevation: 2,
   },
 })
